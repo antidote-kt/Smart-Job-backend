a97fb6731357d3772329b24844a432ce2a76b0ba
antidote <3226594227@qq.com>
2025-09-09 00:38:08 +0800

    工作流CI/CD

--- src/test/java/com/sysu/smartjob/SmartJobBackendApplicationTests.java
+++ src/test/java/com/sysu/smartjob/SmartJobBackendApplicationTests.java
@@ -1,24 +1,25 @@
 package com.sysu.smartjob;
 
-import com.sysu.smartjob.ai.ChatService;
+import com.sysu.smartjob.config.TestConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Flux;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@ContextConfiguration(classes = TestConfig.class)
 class SmartJobBackendApplicationTests {
-	@Autowired
-	private ChatService chatService;
 
 	@Test
 	void contextLoads() {
-
-
+		// 测试 Spring 上下文是否能正常加载
 	}
-	@Test
-	void chatTest() {
-		String res = chatService.chat(1, "你是谁");
-		System.out.println(res);
-	}
+	
+	// 注释掉需要外部依赖的测试
+	// @Test
+	// void chatTest() {
+	// 	String res = chatService.chat(1, "你是谁");
+	// 	System.out.println(res);
+	// }
 }
